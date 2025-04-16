728c6b15484e015b003508d64c1647312d804e70
edwin-vargas <edwin548vargas@gmail.com>
2025-04-16 00:35:01 -0600

    cors 2.0 the revenge

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import cors from 'cors'; // Import the cors middleware
 
 const app = express();
 
-// Enable CORS for all origins (for development - be more specific in production)
-app.use(cors());
+// Allow CORS only from http://200.68.166.160
+const corsOptions = {
+    origin: 'http://200.68.166.160',  // You can also allow HTTPS if needed.
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Hola, este es un texto simple.');
