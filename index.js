c57f7214d096bf2a871a4596c272cf521a133e2a
edwin-vargas <edwin548vargas@gmail.com>
2025-04-15 23:12:59 -0600

    testing web server htmk visualization

---
+++ index.js
@@ -0,0 +1,13 @@
+// server.js
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola, este es un texto simple.');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor Express escuchando en http://localhost:${PORT}`);
+});
