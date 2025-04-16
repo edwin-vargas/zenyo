ba41a5635be84d061acd0e031cce1d73a55f6b5f
edwin-vargas <edwin548vargas@gmail.com>
2025-04-15 23:35:38 -0600

    CORSSSS

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-// server.js
 import express from 'express';
+import cors from 'cors'; // Import the cors middleware
 
 const app = express();
+
+// Enable CORS for all origins (for development - be more specific in production)
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hola, este es un texto simple.');
